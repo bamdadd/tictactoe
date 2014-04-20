5b2d4211a44b89ac34d6ce95f54b692ad2d20474
Bamdad Dashtban <Bamdad@thoughtworks.com>
2014-04-20 23:13:44 +0100

    @bamdadd : adding players object

---
+++ src/players.js
@@ -0,0 +1,12 @@
+var tictactoe = tictactoe || {};
+
+
+tictactoe.Players = function(){
+    var x = 'X';
+    var o = 'O';
+    
+    return {
+        x: x,
+        o: o
+    }
+}
---
+++ test/unit/players.spec.js
@@ -0,0 +1,11 @@
+describe('Players', function(){
+    var players;
+    beforeEach(function(){
+        players = tictactoe.Players();
+    })
+
+   it('should know about x and o', function(){
+       expect(players.x).toEqual('X');
+       expect(players.o).toEqual('O');
+   }) ;
+});
