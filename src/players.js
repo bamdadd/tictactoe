ec3e8c25f235b769c961c98cb062dd622505a271
Bamdad Dashtban <Bamdad@thoughtworks.com>
2014-04-20 23:45:33 +0100

    @bamdadd : removed the state logic from players

--- src/players.js
+++ src/players.js
@@ -1,18 +1,12 @@
 var tictactoe = tictactoe || {};
 
 
-tictactoe.Players = function(gameState){
+tictactoe.Players = function(){
     var x = 'X';
     var o = 'O';
-
-    function next(){
-        var current_turn = gameState.turn();
-        return (current_turn== x)? o : x;
-    }
 
     return {
         x: x,
         o: o,
-        next: next
     }
 }
--- test/unit/players.spec.js
+++ test/unit/players.spec.js
@@ -1,10 +1,8 @@
 describe('Players', function(){
     var players;
-    var gameState;
 
     beforeEach(function(){
-        gameState = jasmine.createSpyObj('GameState', ['turn']);
-        players = tictactoe.Players(gameState);
+        players = tictactoe.Players();
     })
 
    it('should know about x and o', function(){
@@ -12,13 +10,5 @@ describe('Players', function(){
        expect(players.o).toEqual('O');
    }) ;
 
-   it('should provide the next player', function(){
-       gameState.turn.and.returnValue(players.x);
-       players= tictactoe.Players(gameState);
 
-       var nextPlayer = players.next();
-
-       expect(gameState.turn).toHaveBeenCalled();
-       expect(nextPlayer).toEqual(players.o);
-   });
 });
